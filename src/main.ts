585726398bfad30b3b5065f6438a11eba666fd15
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2022-06-22 16:31:27 +0200

    test: inital version

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,75 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {context, getOctokit} from '@actions/github'
+
+const regexMatchIssue = /(AB#[0-9]+)/
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    if (!context.payload.pull_request) {
+      return
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const token: string = core.getInput('token')
 
-    core.setOutput('time', new Date().toTimeString())
+    const github = getOctokit(token)
+
+    const prTitle = context.payload.pull_request.title
+
+    const bodyIssues = await readAllIssues(
+      context.payload.pull_request.body || ''
+    )
+
+    const commits = await github.rest.pulls.listCommits({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number
+    })
+
+    /*const { data: diff } = await github.rest.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number,
+      mediaType: {
+        format: "patch",
+      },
+    })*/
+
+    for (const issue of bodyIssues) {
+      core.debug(`Found issue ${issue} in body`)
+    }
+
+    for (const commit of commits.data) {
+      const commitMessage = commit.commit.message
+      const commitIssues = await readAllIssues(commitMessage || '')
+      for (const issue of commitIssues) {
+        core.debug(`Found issue ${issue} in commit message`)
+      }
+    }
+
+    const newTitle = replaceIssueNumbers(prTitle, bodyIssues)
+    core.debug(`New title: ${newTitle}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+async function readAllIssues(body: string): Promise<string[]> {
+  const matches = body.match(regexMatchIssue)
+
+  if (!matches) {
+    return []
+  }
+
+  return matches.map(match => match.toString())
+}
+
+async function replaceIssueNumbers(
+  prTitle: string,
+  issues: string[]
+): Promise<string> {
+  const titleWithoutIssues = prTitle.replace(/\(\)/, '')
+
+  return titleWithoutIssues + issues.map(issue => `${issue}`).join(', ')
 }
 
 run()
